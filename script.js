9283e3a9368c1b0374671643f6082ac654bb1e1f
Nizar7zak <nizar7zakout@gmail.com>
2021-06-30 17:18:43 +0300

    final changes

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 const sectionWelcome = document.querySelector(".welcome-page")
 const sectionLogin = document.querySelector(".login-page")
 const sectionQuestions = document.querySelector(".questions")
+const sectionScore = document.querySelector(".result-page")
 
 const stepShow = document.querySelector(".where-step")
+const finalScore = document.querySelector("#final-score")
+const firstInTable = document.querySelector(".first-one")
+const hisScore = document.querySelector(".his-score")
+const playerName = document.querySelector(".player-name")
+
+
 
 const question1 = document.querySelector(".sec-one")
 const question2 = document.querySelector(".sec-two")
@@ -18,38 +25,67 @@ const question10 = document.querySelector(".sec-ten")
 
 const barNextAndResult = document.querySelector(".bar")
 
+
 const arrQuesions = [question1,question2,question3,question4,question5,question6,
-    question7,question8,question9,question10];
-
-const btnStartPlay = document.querySelector(".start-play");
-const btnNextToQuestions = document.querySelector(".next");
-const btnNextQuestion = document.querySelector(".next-question")
-
-btnStartPlay.addEventListener("click", () => {
-    sectionWelcome.style.display = "none";
-    sectionLogin.style.display = "flex";
-})
-
-btnNextToQuestions.addEventListener("click", () => {
-    sectionLogin.style.display = "none";
-    sectionQuestions.style.display = "flex";
-})
-
-
-let step = 1;
-btnNextQuestion.addEventListener("click", () => {
-    step++;
-    stepShow.innerHTML = `${step}/10`;
-
+    question7,question8,question9,question10];    
     
-    arrQuesions[step - 2].style.display = "none";
-    arrQuesions[step - 1].style.display = "flex";
-
-
-    // if (step > 10) {
-    //     sectionQuestions.style.display = "none";
-    //     barNextAndResult.style.display = "none";
-    // }
-
-
-})
+    const btnStartPlay = document.querySelector(".start-play");
+    const btnNextToQuestions = document.querySelector(".next");
+    const btnNextQuestion = document.querySelector(".next-question")
+    
+    btnStartPlay.addEventListener("click", () => {
+        sectionWelcome.style.display = "none";
+        sectionLogin.style.display = "flex";
+    })    
+    
+    btnNextToQuestions.addEventListener("click", () => {
+        sectionLogin.style.display = "none";
+        sectionQuestions.style.display = "flex";
+    })    
+    
+    
+    
+    
+    let result = 0;
+    function checkChoose (e){
+        if (e.className === "text-opt correct") {
+            result++;
+            finalScore.innerHTML = `${result}/10`;
+            hisScore.innerHTML = `${result}/10`;
+            
+        }     
+    }    
+    
+    function changeValue(e) {
+        firstInTable.innerHTML = e.value;
+    }    
+    
+    
+    
+    let step = 1;
+    btnNextQuestion.addEventListener("click", () => {
+        step++;
+        if (step === 11)  {
+            sectionQuestions.style.display = "none";
+            sectionScore.style.display = "flex";
+            
+            
+        } else {
+            
+            stepShow.innerHTML = `${step}/10`;
+            arrQuesions[step - 2].style.display = "none";
+            arrQuesions[step - 1].style.display = "flex";
+            
+        }    
+        
+    })    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
